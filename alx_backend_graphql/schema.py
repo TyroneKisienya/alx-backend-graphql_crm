0832ec005e1845c9c5895b69e6ee222d8016e0d2
Skai <tyronekisienya01@gmail.com>
2025-11-07 08:32:39 +0300

    restructure

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,8 @@
 import graphene
+
 from crm.schema import CRMQuery
+
 class Query(CRMQuery, graphene.ObjectType):
-    hello = graphene.String(default_value = "Hello, GraphQL!")
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
