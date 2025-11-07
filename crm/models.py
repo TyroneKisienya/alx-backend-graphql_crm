c617d52d3e266bca505640c457a66c2a1897c94d
Skai <tyronekisienya01@gmail.com>
2025-11-07 12:46:37 +0300

    crm/schema

--- crm/models.py
+++ crm/models.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 # Create your models here.
 class Customer(models.Model):
-    name= models.CharField(max_length=128)
+    name= models.CharField(max_length=100)
     email=models.EmailField(unique=True)
 
     phone= models.CharField(max_length=20, null= True, validators=[ RegexValidator(
@@ -18,7 +18,7 @@ class Customer(models.Model):
         return f"{self.name} ({self.email})"
     
 class Product(models.Model):
-    name= models.CharField(max_length=128)
+    name= models.CharField(max_length=100)
 
     price= models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(
         Decimal("0.01")
