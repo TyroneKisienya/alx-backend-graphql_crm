4280c2044b0d815fb237adbec884230288ce92dc
Skai <tyronekisienya01@gmail.com>
2025-11-13 09:44:00 +0300

    copy of settings and edited cron.py to capture gql and client

--- crm/cron.py
+++ crm/cron.py
@@ -1,21 +1,32 @@
 from datetime import datetime
 import requests
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 def log_crm_heartbeat():
     log_file= "/tmp/crm_heartbeat_log.txt"
     timestamp= datetime.now().strftime('%d-%m-%Y-%H:%M:%S')
 
+    transport= RequestsHTTPTransport(
+        url="http://localhost:8000/graphql",
+        verify=True,
+        retries=3,
+    )
+
 # not amust
-    graphql_url= "http://localhost:8000/graphql"
-    query= {"query": "{hello}"}
+    client = Client(transport=transport,fetch_schema_from_transport=False)
+    query= gql(
+        """
+            query{
+            hello
+            }
+        """
+    )
 
     try:
-        response= requests.post(graphql_url, json=query, timeout=5)
-        if response.status_code == 200:
-            msg=f"{timestamp} CRM is okay"
-        else:
-            msg= f"{timestamp} CRM heartbeat - Graphql responded with {response.status_code}"
-    
+        response= client.execute(query)
+        hello_message= response.get("hello", "No response")
+        msg= f"{timestamp} CRM is alive Saying {hello_message}"
     except Exception as e:
         msg= f"{timestamp} CRM heartbeat - Graphql check failed: {e}"
 
